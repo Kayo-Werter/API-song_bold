8a233db4fb412704424893e7d8911be40af3aa63
Kayo-Werter <kayo.werter1@gmail.com>
2024-05-16 11:18:38 -0300

    users and settings cors

--- review/viewsets.py
+++ review/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from review.models import Review
 from review.serializers import ReviewSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated,]
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
