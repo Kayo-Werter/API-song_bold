82a6543deabab5fc5ee05cd4e32c6b5e8dca971f
Kayo-Werter <kayo.werter1@gmail.com>
2024-06-04 15:47:36 -0300

    atualizacoes frontend

--- vite-project/src/components/navbar/navbar.jsx
+++ vite-project/src/components/navbar/navbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import logo from './logo.png';
+import pesquisarIcon from './pesquisar.png';
+import './navbar.css'
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Login</Link></li>
-                <li><Link to="/inicio">Inicio</Link></li>
-                {/* Adicione outros links conforme necessário */}
-            </ul>
+        <nav className="navbar navbar-light bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Logo" style={{ width: '100px', height: '50px' }}/>
+                </a>
+                <div className="d-flex align-items-center">
+                    <input type="search" name="pesquisar" className="pesquisar" placeholder="Pesquisar"/>
+                    <button type="submit" className="btn__pesquisar" >
+                        <img src={pesquisarIcon} alt="Ícone de busca" style={{ width:'20px', height:'44px' }}/>
+                    </button>
+                </div>
+                <div>
+                    <button className="btn__perfil"><img src="" alt=""/></button>
+                </div>
+            </div>
         </nav>
+        
     );
 };
 
