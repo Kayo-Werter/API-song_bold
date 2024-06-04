02093de422fcb6cbf65295d2c50c1f026220bf04
Kayo-Werter <kayo.werter1@gmail.com>
2024-06-04 16:16:02 -0300

    att navbar

--- vite-project/src/components/navbar/navbar.jsx
+++ vite-project/src/components/navbar/navbar.jsx
@@ -1,18 +1,20 @@
-import logo from './logo.png';
-import pesquisarIcon from './pesquisar.png';
+import logo from './assets/logo.png';
+import pesquisarIcon from './assets/pesquisar.png';
 import './navbar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-light bg-dark">
             <div className="container">
                 <a className="navbar-brand" href="#">
-                    <img src={logo} alt="Logo" style={{ width: '100px', height: '50px' }}/>
+                    <img src={logo} alt="Logo" className="logo"/>
                 </a>
-                <div className="d-flex align-items-center">
+                <div className="input_pesquisar">
                     <input type="search" name="pesquisar" className="pesquisar" placeholder="Pesquisar"/>
                     <button type="submit" className="btn__pesquisar" >
-                        <img src={pesquisarIcon} alt="Ícone de busca" style={{ width:'20px', height:'44px' }}/>
+                        <img src={pesquisarIcon} alt="Ícone de busca" style={{ width:'20px', height:'20px' }}/>
                     </button>
                 </div>
                 <div>
