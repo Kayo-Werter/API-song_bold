7b5f0f84f39f0a160719a258ed09b4d08715281d
Kayo-Werter <kayo.werter1@gmail.com>
2024-06-06 16:53:06 -0300

    finalização do CRUD sem o front

--- vite-project/src/components/video/video.jsx
+++ vite-project/src/components/video/video.jsx
@@ -1,10 +1,42 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import ReactPlayer from 'react-player';
+
+import api from '../../api'
+
+import Navbar from '../navbar/navbar';
+import VideosMenores from '../VideosMenores/VideosMenores';
+
 import './video.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../navbar/navbar';
-import tumb from './transferir.jpeg';
+
 
 
 function Video() {
+
+    const { videoId } = useParams()
+    const [title, setTitle] = useState('')
+    const [url, setUrl] = useState('')
+    const [category, setcategory] = useState('')
+    const [release_date, setReleaseDate] = useState('')
+    const [description, setDescription] = useState('')
+
+
+    useEffect(() => {
+        // Carrega os detalhes do produto
+        api.get(`/v1/video/${videoId}/`)
+          .then(response => {
+            setTitle(response.data.title)
+            setUrl(response.data.url)
+            setcategory(response.data.category)
+            setReleaseDate(response.data.release_date)
+            setDescription(response.data.description)
+          })
+          .catch(error => {
+            console.error('Erro ao buscar detalhes do produto:', error)
+          })
+      }, [videoId])
+
     return (
         <div>
             <div>
@@ -12,43 +44,25 @@ function Video() {
             </div>
             <div className="container text-center">
                 <div className="row">
-                    <div className="col-sm-8">
-                        <div className='video_principal_settings'>
-                            <img className='video_principal' src={tumb} alt="" />
-                        </div>
-                        
-                        <div className='descricao_principal'>
-                        <span >
-                            Como fazer sua Tumbr profissional</span>
-                        </div>
-                        <div className='descricao_principal'>
-                            <button className="btn__perfil" style={{ width: '30px', height: '30px' }}><img src="" alt="" /></button>
-                            <span>TumbrsBRgb </span>
-                            <span>há 3dias</span>
+                    <div className="col">
+                        <div className="col-sm-8">
+                            <div className='video_principal_settings'>
+                                <ReactPlayer controls={true} url={url} />
+                            </div>
+                            <div className='descricao_principal'>
+                                <h3 to={`/video/${videoId}/`} className="produto-link">
+                                    <span>{title}</span>
+                                </h3>
+                            </div>
+                            <div className='descricao_principal'>
+                                <h4>{category.name}</h4><br></br>
+                                <h5>{release_date}</h5>
+                                <span>{description}</span>
+                            </div>
                         </div>
                     </div>
                     <div className="col-sm-4">
-                        <div className="video">
-                            <img className="info_video_tumb" src={tumb} alt="tumb" />
-                            <div className="info_video">
-                                <div className="info_video_descricao">
-                                    <h6>Como fazer sua Tumbr profissional</h6>
-                                    <h8>TumbrsBRgb </h8>
-                                    <h8>há 3 dias</h8>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="video">
-                            <img className="info_video_tumb" src={tumb} alt="tumb" />
-                            <div className="info_video">
-                                <div className="info_video_descricao">
-                                    <span>Como fazer sua Tumbr profissional</span>
-                                    <span>TumbrsBRgb </span>
-                                    <span>há 3 dias</span>
-                                </div>
-                            </div>
-                        </div>
+                        <VideosMenores />
                     </div>
                 </div>
             </div>
